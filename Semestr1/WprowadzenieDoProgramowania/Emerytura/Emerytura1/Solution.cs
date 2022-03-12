e211d9ea5d43271f8c064a848aced508bc3c2177
Karmelova <kamila.weronika.marcinek@gmail.com>
2022-03-13 00:16:23 +0100

    Add solution.cs

---
+++ Semestr1/WprowadzenieDoProgramowania/Emerytura/Emerytura1/Solution.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string lastName = Console.ReadLine();
+        int currentAge = int.Parse(Console.ReadLine());
+        int retirementAge = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Witaj, {lastName}!");
+
+        if (currentAge < 0 || retirementAge < 0)
+            Console.WriteLine("Wiek nie mo¿e byæ ujemny!");           
+        else if (currentAge < retirementAge)
+            Console.WriteLine($"Do emerytury brakuje Ci {retirementAge - currentAge} lat!");            
+        else
+            Console.WriteLine("Jesteœ emerytem!");
+            
+        return;
+
+    }
+
+}
